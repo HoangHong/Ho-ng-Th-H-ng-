57d73604d9d6acdb0a1b9e92ed9d0b6894f7b984
hoanghong <hoanghong97spkt@gmail.com>
2019-04-08 16:00:19 +0700

    unit13

--- unit12/js/baitap.js
+++ unit12/js/baitap.js
@@ -52,3 +52,57 @@
                 email:"Làm ơn nhập email",
             }
         });
+
+ 
+var currentshow = 0;
+showItems(currentshow);
+
+ 
+ function showItems(n) {
+          var x = $(".content__body");
+          x[n].style.display = "block";
+          
+          if (n == 0) {
+            document.getElementById("prev").style.display = "none";
+          } 
+          else {
+            document.getElementById("prev").style.display = "inline";
+          }
+          if (n == (x.length - 1)) {
+            document.getElementById("next").innerHTML = "Finish";
+          }
+          else {
+            document.getElementById("next").innerHTML = "Next";
+          }
+          fixStepIndicator(n);
+  }
+    function next(n) {
+ 
+  var x = document.getElementsByClassName("content__body");
+  
+  if (n == 1) 
+    return false;
+ 
+  x[currentshow].style.display = "none";
+
+  currentshow = currentshow + n;
+
+  if (currentshow >= x.length) {
+
+    alert("Hoàn thành!");
+    document.getElementsByClassName("content").submit();
+    return false;
+    }
+  showItems(currentshow);
+}
+
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByTagName("h3");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
